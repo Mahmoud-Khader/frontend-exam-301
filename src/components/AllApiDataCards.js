7d5965fa56b36f82e145fa5807e97ac2ae709c49
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-08-03 12:51:46 +0300

    adding some css

--- src/components/AllApiDataCards.js
+++ src/components/AllApiDataCards.js
@@ -4,15 +4,15 @@ import { Card, Button } from 'react-bootstrap';
 export class AllApiDataCards extends Component {
     render() {
         return (
-            <div>
+            <div style={{ backgroundColor: '#8c8181' }} >
                 {this.props.showData && this.props.dataList.map((item, index) => {
                     return (
-                        <Card style={{ width: '18rem', display: 'inline-block' }}>
+                        <Card style={{ width: '18rem', display: 'inline-block', margin:'20px' }}>
                             <Card.Img variant="top" src={item.imageUrl} />
                             <Card.Body>
                                 <Card.Title>{item.title}</Card.Title>
 
-                                <Button onClick={() => { this.props.createFav(item) }} variant="primary">Add To Fav</Button>
+                                <Button onClick={() => { this.props.createFav(item) }} variant="warning">Add To Fav</Button>
                             </Card.Body>
                         </Card>
                     )
--- src/components/AllFavDataCards.js
+++ src/components/AllFavDataCards.js
@@ -4,16 +4,16 @@ import { Card, Button } from 'react-bootstrap';
 export class AllFavDataCards extends Component {
     render() {
         return (
-            <div>
+            <div style={{ backgroundColor: '#8c8181' }}>
                 {this.props.showData && this.props.dataList.map((item, index) => {
                     return (
-                        <Card style={{ width: '18rem', display: 'inline-block' }}>
+                        <Card style={{ width: '18rem', display: 'inline-block',margin:'20px' }}>
                             <Card.Img variant="top" src={item.imageUrl} />
                             <Card.Body>
                                 <Card.Title>{item.title}</Card.Title>
 
-                                <Button onClick={() => { this.props.deleteData(index) }} variant="primary">Delete</Button>
-                                <Button onClick={() => { this.props.handleShow(index) }} variant="primary">Update</Button>
+                                <Button onClick={() => { this.props.deleteData(index) }} variant="danger">Delete</Button>
+                                <Button onClick={() => { this.props.handleShow(index) }} variant="success">Update</Button>
                             </Card.Body>
                         </Card>
                     )
